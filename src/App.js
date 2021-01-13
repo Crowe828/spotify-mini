23e8dc6951a679e739491be96f80ef02e6ca9374
frillic <69489498+crowe828@users.noreply.github.com>
2021-01-13 17:06:35 -0500

    set up hook for logging in and grabbing user's access token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
+import { getTokenFromUrl } from "./Spotify";
 import Login from "../src/Components/Login";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", _token);
+  }, []);
+
+  return <div className="App">{token ? <h1>Logged in</h1> : <Login />}</div>;
 }
 
 export default App;
