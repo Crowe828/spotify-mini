8d8fa305523882061278a4bb70dced48b73752cc
frillic <69489498+crowe828@users.noreply.github.com>
2021-01-14 17:06:46 -0500

    built player finctionality

---
+++ src/Components/Header/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Search } from "@material-ui/icons";
+import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "../../Data/DataLayer";
+import "./style.css";
+
+function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Search />
+        <input placeholder="Search for artists, songs, or albums" type="text" />
+      </div>
+      <div className="header__right">
+        <Avatar src={user?.images[0]} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/Components/Player/index.js
+++ src/Components/Player/index.js
@@ -1,11 +1,18 @@
 import "./style.css";
+import Sidebar from "../Sidebar";
+import Body from "../Body";
+import Footer from "../Footer";
 
-function Player() {
-    return (
-        <div>
-            
-        </div>
-    )
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Player;
